33a9ceb37a061ea3e5d922e413dd07f16fa643f0
acgbm <acgabrielm@gmail.com>
2025-05-31 04:02:36 +0800

    update

--- staff-management/accounts/serializers.py
+++ staff-management/accounts/serializers.py
@@ -1,16 +1,30 @@
-from django.contrib.auth.models import User
+
 from rest_framework import serializers
+from fileupload.models import CustomUser
+
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+    email = serializers.EmailField(required=True)
+    photo = serializers.ImageField(required=False, allow_null=True)
+
 
     class Meta:
-        model = User
-        fields = ['username', 'password']
+        model = CustomUser
+        fields = ['username', 'password', 'email', 'photo']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def validate_photo(self, photo):
+        if photo:
+            if photo.size > 2 * 1024 * 1024:
+                raise serializers.ValidationError("Image size should not exceed 2MB.")
+            if not photo.content_type in ['image/jpeg', 'image/png', 'image/webp']:
+                raise serializers.ValidationError("Only JPEG, PNG, and WebP images are allowed.")
+        return photo
 
     def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            password=validated_data['password']
-        )
+        password = validated_data.pop('password')
+        user = CustomUser(**validated_data)
+        user.set_password(password)
+        user.save()
         return user
--- staff-management/accounts/views.py
+++ staff-management/accounts/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from .serializers import RegisterSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
-from django.contrib.auth.models import User
+
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
